158f10673301db4cd4540b583d6e77b9b10a93bc
Yong Shean CHONG <yong.shean.chong@comerge.net>
2021-06-11 23:19:03 +0800

    Remove url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 import 'routing.dart';
 
 void main() {
-  setPathUrlStrategy();
   runApp(WeatherApp());
 }
 
